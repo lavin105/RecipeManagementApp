b7a8d6eca1bb6aca832445ea2f9f9160ad6f3fe1
lavin105 <lavin105@mail.chapman.edu>
2018-10-31 12:24:26 -0700

    Finished signup and login layouts and created activities for them

---
+++ app/src/main/java/lavin105/recipemanager/Login.java
@@ -0,0 +1,43 @@
+package lavin105.recipemanager;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Login extends Activity {
+    Button loginBtn;
+    EditText password, email;
+    TextView toSignUp;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+        loginBtn=findViewById(R.id.login);
+        password=findViewById(R.id.password_input_login);
+        email=findViewById(R.id.email_input_login);
+        toSignUp=findViewById(R.id.to_signup);
+
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toMain= new Intent(Login.this,RecipeGrid.class);
+                startActivity(toMain);
+            }
+        });
+
+        toSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toSignup= new Intent(Login.this,SignUp.class);
+                startActivity(toSignup);
+            }
+        });
+
+    }
+}
--- app/src/main/java/lavin105/recipemanager/SignUp.java
+++ app/src/main/java/lavin105/recipemanager/SignUp.java
@@ -6,19 +6,36 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SignUp extends Activity {
     Button signUp;
+    EditText email, password;
+    TextView goToLogin;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
+
         signUp=findViewById(R.id.sign_up);
+        email=findViewById(R.id.email_input);
+        password=findViewById(R.id.password_input);
+        goToLogin=findViewById(R.id.to_login);
+
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(SignUp.this, RecipeGrid.class);
                 startActivity(i);
+            }
+        });
+
+        goToLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toLogin = new Intent(SignUp.this, Login.class);
+                startActivity(toLogin);
             }
         });
     }
