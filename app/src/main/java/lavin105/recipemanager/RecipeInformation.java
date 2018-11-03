dc02ce75b1060da337f1566b25a748d9daef9e92
lavin105 <lavin105@mail.chapman.edu>
2018-11-03 16:00:40 -0700

    added setup code for recipe information

--- app/src/main/java/lavin105/recipemanager/RecipeInformation.java
+++ app/src/main/java/lavin105/recipemanager/RecipeInformation.java
@@ -3,13 +3,16 @@ package lavin105.recipemanager;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Adapter;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.RadioGroup;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -23,6 +26,9 @@ public class RecipeInformation extends Activity {
     ArrayAdapter<String> adapter;
     String[] ingred;
     String ing;
+    RatingBar theRating;
+    ImageView recipePicture;
+    FloatingActionButton toYoutube, toWeb, toShare;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +37,22 @@ public class RecipeInformation extends Activity {
         recipeName=findViewById(R.id.recipe_name);
         recipeInstructions=findViewById(R.id.instruction_description);
         ingredientsList=findViewById(R.id.ingredients_list);
+        theRating=findViewById(R.id.the_recipe_rating);
+        theRating.setRating(2);
+        recipePicture=findViewById(R.id.the_recipe_picture);
+        toShare=findViewById(R.id.share);
+        toWeb=findViewById(R.id.web);
+        toYoutube=findViewById(R.id.youtube);
+
+        //TODO
+        //Add getIntent here
+
+
+
+
+
+
+
         ing="ingredient1,ingredient2,ingredient3,ingredient4";
         ingred=ing.split(",");
         theIngredients= new ArrayList<String>(Arrays.asList(ingred));
@@ -53,6 +75,25 @@ public class RecipeInformation extends Activity {
                   recipeInstructions.setVisibility(View.INVISIBLE);
 
               }
+            }
+        });
+
+        toYoutube.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Youtube");
+            }
+        });
+        toWeb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Web");
+            }
+        });
+        toShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Share");
             }
         });
     }
